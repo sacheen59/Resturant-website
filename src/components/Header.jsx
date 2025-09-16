7273c0acb79c19ed2534d081547b99abcf3a8048
sachin barali <baralisachin472@gmail.com>
2025-09-16 08:42:00 +0545

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+import logoImage from "../assets/images/logo.jpg";
+
+const Header = () => {
+  return (
+    <h1 className="text-[40px] text-center">This is Header</h1>
+  );
+};
+
+
+export default Header;
